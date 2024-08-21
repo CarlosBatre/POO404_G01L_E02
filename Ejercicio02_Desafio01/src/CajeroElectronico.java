c1abdb03efb723b079ea538d8b5389e9ef52a37d
RB230288 <carlosbatres.gg@gmail.com>
2024-08-21 10:55:59 -0600

    Consiga, Transferencias y correccion de errores y validaciones

--- Ejercicio02_Desafio01/src/CajeroElectronico.java
+++ Ejercicio02_Desafio01/src/CajeroElectronico.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 public class CajeroElectronico {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         //Arreglo con las cuentas y su respectivo saldo
         int[][] cuentas = {
                 {1000001, 400},
                 {1000002, 200},
                 {1000004, 500}
         };
+
         //Acceso con el numCuenta
         int numCuenta;
         int saldo = -1;
@@ -18,9 +20,10 @@ public class CajeroElectronico {
                 numCuenta = Integer.parseInt(numCuentaIngreso);
                 break;
             } catch (NumberFormatException e){
-                System.out.println("\nSolo se permiten numero. Intentelo de nuevo. :)");
+                System.out.println("\nSolo se permiten numeros. Intentelo de nuevo. :)");
             }
         }
+
         //Comparacion si existe la cuenta
         for (int[] cuenta : cuentas) {
             if (cuenta[0] == numCuenta) {
@@ -30,11 +33,13 @@ public class CajeroElectronico {
                 break;
             }
         }
+
         //Manejo de error si no encuentra la cuenta
         if (saldo == -1) {
             System.out.println("\nNúmero de cuenta no encontrado. Intentarlo mas tarde!!");
             return;
         }
+
         //menu banca electro
         while (true) {
             System.out.println("\nSeleccione una opcion:");
@@ -44,45 +49,80 @@ public class CajeroElectronico {
             System.out.println("4. Transferencias");
             System.out.println("5. Salir");
             int opcion = 0;
+
             try {
                 System.out.print("\nOpción: ");
                 opcion = scanner.nextInt();
-            } catch (Exception e) {
-                System.out.println("Ingrese un número válido. I     ntentarlo de nuevo :).");
-                scanner.nextLine();
-                continue;
-            }
-            if (opcion == 5) {
-                System.out.println("Muchas Gracias por usar nuestros servicios. ¡Hasta luego!ඞ");
-                break;
-            }
-            //procedimientos del menu banca
-            switch (opcion){
-                //consulta saldo
-                case 1:
-                    System.out.println("\nSaldo actual de la cuenta #" + numCuenta + " es de $" + saldo);
-                    break;
-                //retiro de fondos
-                case 2:
-                    try {
-                        System.out.print("\nIngrese la cantidad a retirar: ");
-                        int retiro = scanner.nextInt();
-                        if (retiro <= saldo) {
-                            saldo -= retiro;
-                            System.out.println("Retiro exitoso. Su saldo actual es: $" + saldo);
-                        } else {
-                            System.out.println("Lo siento....fondos insuficientes :(.");
+
+                //procedimientos del menu banca
+                switch (opcion) {
+
+                    //consulta saldo
+                    case 1:
+                        System.out.println("\nSaldo actual de la cuenta #" + numCuenta + " es de $" + saldo);
+                        break;
+
+                    //retiro de fondos
+                    case 2:
+                        try {
+                            System.out.print("\nIngrese la cantidad a retirar: ");
+                            int retiro = scanner.nextInt();
+                            if (retiro <= saldo) {
+                                saldo -= retiro;
+                                System.out.println("Retiro exitoso. Su saldo actual es: $" + saldo);
+                            } else {
+                                System.out.println("Lo siento....fondos insuficientes :(.");
+                            }
+                        } catch (Exception e) {
+                            System.out.println("Solo se permiten numeros. Intentelo de nuevo. :)");
+                            scanner.nextLine();
                         }
-                    } catch (Exception e) {
-                        System.out.println("Ingrese una cantidad valida");
-                        scanner.nextLine();
-                    }
-                    break;
+                        break;
 
+                    //Consigna
+                    case 3:
+                        try {
+                            System.out.print("\nIngrese la cantidad para realizar la consigna: ");
+                            int consigna = scanner.nextInt();
+                            saldo += consigna;
+                            System.out.println("Consignación exitosa. Saldo actual: $" + saldo);
+                        } catch (Exception e) {
+                            System.out.println("Solo se permiten numeros. Intentelo de nuevo. :)");
+                            scanner.nextLine();
+                        }
+                        break;
 
-                default:
-                    System.out.println("Opción no válida. Inténtelo de nuevo.");
-                    break;
+                    //Transferencias
+                    case 4:
+                        try {
+                            System.out.print("\nNumero de cuenta a transferir: ");
+                            int cuentaTransferencia = scanner.nextInt();
+                            System.out.print("Ingrese la cantidad a transferir: ");
+                            int transferencia = scanner.nextInt();
+                            if (transferencia <= saldo) {
+                                saldo -= transferencia;
+                                System.out.println("Transferencia exitosa. Su saldo actual es: $" + saldo);
+                            } else {
+                                System.out.println("Lo siento....fondos insuficientes :(.");
+                            }
+                        } catch (Exception e) {
+                            System.out.println("Solo se permiten numeros. Intentelo de nuevo. :)");
+                            scanner.nextLine();
+                        }
+                        break;
+
+                    //Salir
+                    case 5:
+                        System.out.println("Muchas Gracias por usar nuestros servicios. ¡Hasta luego!ඞ");
+                        return;
+                    default:
+                        System.out.println("Opción no válida. Inténtelo de nuevo.");
+                        break;
+                }
+
+            }   catch (Exception e) {
+                System.out.println("Ingrese un número 1-5. Intentarlo de nuevo :).");
+                scanner.nextLine();
             }
         }
 
