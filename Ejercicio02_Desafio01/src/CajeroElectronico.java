8162db7d95d371d6022a0f9e3b6165f4e3997c90
RB230288 <carlosbatres.gg@gmail.com>
2024-08-21 09:49:18 -0600

    Validaciones Clase CajeroElectronico

--- Ejercicio02_Desafio01/src/CajeroElectronico.java
+++ Ejercicio02_Desafio01/src/CajeroElectronico.java
@@ -2,26 +2,62 @@ import java.util.Scanner;
 public class CajeroElectronico {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //Arreglo con las cuentas y su respectivo saldo
         int[][] cuentas = {
                 {1000001, 400},
                 {1000002, 200},
                 {1000004, 500}
         };
-        System.out.print("Ingrese el número de cuenta para acceder: ");
-        String numCuentaIngreso = scanner.nextLine();
-        int numCuenta = Integer.parseInt(numCuentaIngreso);
+        //Acceso con el numCuenta
+        int numCuenta;
         int saldo = -1;
+        while (true){
+            System.out.print("\nIngrese el número de cuenta para acceder: ");
+            String numCuentaIngreso = scanner.nextLine();
+            try{
+                numCuenta = Integer.parseInt(numCuentaIngreso);
+                break;
+            } catch (NumberFormatException e){
+                System.out.println("\nSolo se permiten numero. Intentelo de nuevo. :)");
+            }
+        }
+        //Comparacion si existe la cuenta
         for (int[] cuenta : cuentas) {
             if (cuenta[0] == numCuenta) {
                 saldo = cuenta[1];
+                //clear
                 System.out.println("BIENVENIDO A LA BANCA ELECTRÓNICA :D");
                 break;
             }
         }
+        //Manejo de error si no encuentra la cuenta
         if (saldo == -1) {
-            System.out.println("Número de cuenta noenc ontrado. Intentarlo mas tarde!!");
+            System.out.println("\nNúmero de cuenta no encontrado. Intentarlo mas tarde!!");
+            return;
         }
-        while (saldo != -1) {}
+        //menu banca electro
+        while (true) {
+            System.out.println("\nSeleccione una opcion:");
+            System.out.println("1. Consulta de saldo");
+            System.out.println("2. Retiro de fondos");
+            System.out.println("3. Consignaciones");
+            System.out.println("4. Transferencias");
+            System.out.println("5. Salir");
+            int opcion = 0;
+            try {
+                System.out.print("\nOpción: ");
+                opcion = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Ingrese un número válido. I     ntentarlo de nuevo :).");
+                scanner.nextLine();
+                continue;
+            }
+            if (opcion == 5) {
+                System.out.println("Muchas Gracias por usar nuestros servicios. ¡Hasta luego!ඞ");
+                break;
+            }
+
+        }
 
     }
 }
