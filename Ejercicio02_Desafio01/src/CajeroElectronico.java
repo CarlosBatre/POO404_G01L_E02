40463f4a5184d0acb4d354473823a1dc0aa431fd
RB230288 <125234995+CarlosBatre@users.noreply.github.com>
2024-08-20 22:20:17 -0600

    Revert "Acceso con NumCuenta"

--- Ejercicio02_Desafio01/src/CajeroElectronico.java
+++ Ejercicio02_Desafio01/src/CajeroElectronico.java
@@ -1,27 +1,20 @@
-
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class CajeroElectronico {
 
+    private static HashMap<Integer, Integer> Cuenta = new HashMap<>();
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int[][] cuentas = {
-                {1000001, 400},
-                {1000002, 200},
-                {1000004, 500}
-        };
+        Cuenta.put(1000001, 400);
+        Cuenta.put(1000002, 200);
+        Cuenta.put(1000004, 500);
+
         System.out.print("Ingrese el número de cuenta para acceder: ");
-        String numCuentaIngreso = scanner.nextLine();
-        int numCuenta = Integer.parseInt(numCuentaIngreso);
-        boolean cuentaEncontrada = false;
-        for (int[] cuenta : cuentas) {
-            if (cuenta[0] == numCuenta) {
-                cuentaEncontrada = true;
-                System.out.println("BIENVENIDO A LA BANCAELECTRONICA :D");
-                break;
-            }
-        }
-        System.out.println("Número de cuenta no encontrado. Intentarlo mas tarde.");
+        int numeroCuenta = scanner.nextInt();
+
+
+
     }
 }
