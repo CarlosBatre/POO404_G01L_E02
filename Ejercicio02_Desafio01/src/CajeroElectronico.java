23ccec40db62ace8fd664b97ac13afc8654705a0
RB230288 <carlosbatres.gg@gmail.com>
2024-08-21 08:15:44 -0600

    Cambios_ClaseCajeroElectronico

--- Ejercicio02_Desafio01/src/CajeroElectronico.java
+++ Ejercicio02_Desafio01/src/CajeroElectronico.java
@@ -1,11 +1,7 @@
-
 import java.util.Scanner;
-
 public class CajeroElectronico {
-
-    private static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         int[][] cuentas = {
                 {1000001, 400},
                 {1000002, 200},
@@ -14,20 +10,18 @@ public class CajeroElectronico {
         System.out.print("Ingrese el número de cuenta para acceder: ");
         String numCuentaIngreso = scanner.nextLine();
         int numCuenta = Integer.parseInt(numCuentaIngreso);
-        boolean cuentaEncontrada = false;
+        int saldo = -1;
         for (int[] cuenta : cuentas) {
             if (cuenta[0] == numCuenta) {
-                cuentaEncontrada = true;
-                System.out.println("BIENVENIDO A LA BANCAELECTRONICA :D");
+                saldo = cuenta[1];
+                System.out.println("BIENVENIDO A LA BANCA ELECTRÓNICA :D");
                 break;
             }
-            if (!cuentaEncontrada) {
-                System.out.println("Número de cuenta no encontrado. Intentarlo mas tarde!.");
-                System.exit(1);
-            }
         }
-
-
+        if (saldo == -1) {
+            System.out.println("Número de cuenta noenc ontrado. Intentarlo mas tarde!!");
+        }
+        while (saldo != -1) {}
 
     }
 }
