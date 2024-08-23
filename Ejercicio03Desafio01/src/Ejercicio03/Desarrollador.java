a8f940a2e600475689f7df87099ecd1aa216a1cc
RB230288 <carlosbatres.gg@gmail.com>
2024-08-22 19:56:22 -0600

    Clase Desarrollador Ejercicio03

---
+++ Ejercicio03Desafio01/src/Ejercicio03/Desarrollador.java
@@ -0,0 +1,24 @@
+package Ejercicio03;
+
+class Desarrollador extends Empleado {
+    private double extraHoras;
+
+    public Desarrollador(String nombre, double salarioBase, double extraHoras) {
+        super(nombre, salarioBase);
+        this.extraHoras = extraHoras;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return salarioBase + extraHoras * 20; // Ejemplo: pago de $20 por hora extra
+    }
+
+    @Override
+    public void mostrarInfo() {
+        System.out.println("Nombre: " + nombre + " (Desarrollador)");
+        System.out.println("Salario Base: $" + salarioBase);
+        System.out.println("Horas Extras: " + extraHoras + " horas");
+        System.out.println("Salario Total: $" + calcularSalario());
+    }
+}
+
